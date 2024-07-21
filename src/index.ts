9463e68bee340481aeb3c1c14c120428d8940425
NZYK <nozuyukiwork@gmail.com>
2024-07-21 19:15:26 +0900

    getWebGlContext

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 import './styles.css';
+let c: HTMLCanvasElement
+let gl: WebGLRenderingContext;
 
-console.log('This is hello world from TS!')
+onload = (): void => {
+    c = <HTMLCanvasElement>document.getElementById('canvas');
+    c.width = 500;
+    c.height = 300;
+
+    gl = <WebGLRenderingContext>c.getContext('webgl') || <WebGLRenderingContext>c.getContext('experimental-webgl');
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+};
